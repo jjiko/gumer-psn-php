ef4be31cd211292696ff6401d275de0b7a287a6a
Quentin Ligier <quentin.ligier@bluewin.ch>
2016-02-12 09:16:42 +0100

    Get friends presence and manage list offset

--- src/Gumer/PSN/Requests/FriendsListRequest.php
+++ src/Gumer/PSN/Requests/FriendsListRequest.php
@@ -5,7 +5,12 @@ class FriendsListRequest extends AbstractAuthenticatedRequest {
 	/**
 	 * @var string
 	 */
-	protected $uri = 'https://{{region}}-prof.np.community.playstation.net/userProfile/v1/users/{{id}}/friendList?fields=onlineId,avatarUrl,plus,personalDetail,trophySummary&friendStatus=friend';
+	protected $uri = 'https://{{region}}-prof.np.community.playstation.net/userProfile/v1/users/{{id}}/friendList?fields=@default,onlineId,avatarUrl,plus,personalDetail,trophySummary,relation&friendStatus=friend&presenceType=primary&offset={{offset}}';
+
+	/**
+	 * @param array
+	 */
+	protected $params = array('offset' => 0);
 
 	/**
 	 * @param mixed $userId
@@ -14,6 +19,15 @@ class FriendsListRequest extends AbstractAuthenticatedRequest {
 	public function setUserId($userId)
 	{
 		$this->params['id'] = $userId;
+	}
+
+	/**
+	 * @param int $offset
+	 * @return void
+	 */
+	public function setOffset($offset)
+	{
+		$this->params['offset'] = (int)$offset;
 	}
 
 }
